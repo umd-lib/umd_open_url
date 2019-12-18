be642f5fa8001d8ab11ea3d785ea9f7c36c469e7
David P. Steelman <dsteelma@umd.edu>
2019-12-18 09:26:52 -0500

    LIBSEARCH-74. Refactored code to implement "valid?" method
    Refactored UmdOpenUrl::Builder to have a "valid?" method, and to
    ignore and "nil" parameters that are passed in.
    
    Updated the test cases, and added comments.
    
    https://issues.umd.edu/browse/LIBSEARCH-74

--- lib/umd_open_url/builder.rb
+++ lib/umd_open_url/builder.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 module UmdOpenUrl
-  # Builder for fluently creating an OpenUrl with particular parameters
+  # Builder for fluently creating an OpenUrl with particular parameters.
+  #
+  # When passing parameter values, nil values will be ignored.
   class Builder
     def initialize(resolver_url)
       @resolver_url = resolver_url
@@ -9,39 +11,52 @@ module UmdOpenUrl
     end
 
     def issn(issn)
-      @params_map['rft.issn'] = issn
+      @params_map[:issn] = issn unless issn.nil?
       self
     end
 
     def volume(volume)
-      @params_map['rft.volume'] = volume
+      @params_map[:volume] = volume unless volume.nil?
       self
     end
 
     def issue(issue_number)
-      @params_map['rft.issue'] = issue_number
+      @params_map[:issue] = issue_number unless issue_number.nil?
       self
     end
 
     def start_page(start_page)
-      @params_map['rft.spage'] = start_page
+      @params_map[:start_page] = start_page unless start_page.nil?
       self
     end
 
     def publication_date(publication_date)
-      @params_map['rft.date'] = publication_date
+      @params_map[:publication_date] = publication_date unless publication_date.nil?
       self
     end
 
     def custom_param(param_name, param_value)
-      @params_map[param_name] = param_value
+      @params_map[param_name.to_sym] = param_value unless param_value.nil?
       self
+    end
+
+    # Returns true if the given parameters have been populated, false otherwise
+    # List of parameters should be provided as symbols corresponding to the
+    # parameter method names, or custom parameter name
+    def valid?(required_params)
+      required_params.each do |p|
+        return false unless @params_map.key?(p)
+      end
+      true
     end
 
     # Returns a string representing the OpenURL hyperlink from the provided
     # parameters
     def build
-      params = @params_map.map { |k, v| "#{k}=#{v}" }.join('&')
+      params = @params_map.map do |k, v|
+        open_url_key = open_url_params_map[k] || k.to_s
+        "#{open_url_key}=#{v}"
+      end.join('&')
       open_url = @resolver_url + '?' + params
       UmdOpenUrl.logger.debug do
         # Filter out the wskey parameter value
@@ -49,6 +64,19 @@ module UmdOpenUrl
         UmdOpenUrl.logger.debug("UmdOpenUrl::Builder.build - open_url: #{logged_url}")
       end
       open_url
+    end
+
+    private
+
+    # Mapping of param names to OpenUrl query parameter names
+    def open_url_params_map
+      {
+        issn: 'rft.issn',
+        volume: 'rft.volume',
+        issue: 'rft.issue',
+        start_page: 'rft.spage',
+        publication_date: 'rft.date'
+      }
     end
   end
 end
